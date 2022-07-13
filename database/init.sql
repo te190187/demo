1b7754c83c3cffd55d42406cb8f5495169b7efc5
石川 雅述 <190187@jc-21.jp>
2022-07-13 09:14:28 +0900

    Change database

--- database/init.sql
+++ database/init.sql
@@ -1,18 +1,2 @@
 drop database if exists demodb;
 create database demodb;
-
-use demodb;
-
-drop table if exists info;
-create table info(
-        id int not null auto_increment,
-        message varchar(100) not null,
-        primary key (id)
-);
-
-drop table if exists added;
-create table added(
-        id int not null auto_increment,
-        m varchar(100) not null,
-        primary key (id)
-);
---
+++ src/main/resources/db/migration/V1.0.0__migrationtest.sql
@@ -0,0 +1,5 @@
+create table Migtest(
+        id int not null auto_increment,
+        m varchar(100) not null,
+        primary key (id)
+);
